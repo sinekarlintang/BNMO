a362ea05de10b6fc1e3e945fb40536c6d1a38819
Sinekar Lintang <sinekarlintang@gmail.com>
2022-04-11 13:34:15 +0700

    init

---
+++ Function.py
@@ -0,0 +1,6 @@
+def length(x):
+    count = 0
+    for i in x:
+        count += 1
+    return count
+
---
+++ Register.py
@@ -0,0 +1,17 @@
+import csv
+import Function
+
+def
+
+def register():
+    file = open("user.csv","r+", newline='')
+    userWrite = csv.writer(file, delimiter=";")
+
+    id = Function.length(file)
+    nama = input()
+    username = input()
+    password = input()
+    role = "user"
+    saldo = 0
+
+    userWrite.writerow([id,username,nama,password,role,saldo])
